6b2743abce480bc64b8640dd01589fe47cb5b473
GunalanD95 <guna19may2015@gmail.com>
2022-02-19 18:45:04 +0530

    recent changes

--- chat/settings.py
+++ chat/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chat_app',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+ASGI_APPLICATION = "chat.routing.application" 
+
