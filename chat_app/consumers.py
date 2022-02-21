e83790412f25b93f801d5109261be53e3f67e777
GunalanD95 <guna19may2015@gmail.com>
2022-02-21 06:56:33 +0530

    asyn fun

--- chat_app/consumers.py
+++ chat_app/consumers.py
@@ -3,7 +3,17 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        await(
+        self.room_name = self.scope['url_route']['kwargs']['room']
+        self.room_group_name = 'chat_%s' % self.room_name
 
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
         )
